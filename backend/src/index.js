53e7c4b4d77c39b77bc3e7830a50a3bd13915fb3
Davi <davisilvaneves3@gmail.com>
2021-04-19 15:06:33 -0300

    BackEnd v1.0.0

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const app = express();
+// const app = express(); para iniciar a api
+app.use(bodyParser.json()); //para entender json
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors())
+//urlencoded para ele entender parametros via url
+//app.listen = porta que eu quero ouvir
+
+require('./app/controllers/index')(app);
+
+console.log('api ok')
+
+app.listen(3000)
+
